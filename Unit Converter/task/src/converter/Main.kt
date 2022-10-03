38f3a828ecd65a261ede4e5776ddb9da81b718e2
rafaover <rafamsilva@gmail.com>
2022-10-04 10:52:35 +1100

    First Commit

---
+++ Unit Converter/task/src/converter/Main.kt
@@ -0,0 +1,109 @@
+package converter
+
+fun main() {
+    loopcheck@ while (true) {
+        var conversion: Double?
+        print("Enter what you want to convert (or exit): ")
+        val userInput = readln().lowercase()
+
+        if (userInput == "exit") break@loopcheck
+
+        val (originalNumber, originalUnit, convertedUnit) = userInputSplit(userInput)
+
+        // Validating inputs/Enums + exception checks
+        if (originalNumber == null) {
+            println("Wrong number")
+            continue@loopcheck
+        }
+        if (originalUnit == null && convertedUnit != null) {
+            println("Conversion from ??? to ${convertedUnit.singular} is impossible")
+            continue@loopcheck
+        }
+        if (originalUnit != null && convertedUnit == null) {
+            println("Conversion from ${originalUnit.singular} to ??? is impossible")
+            continue@loopcheck
+        }
+        if (originalUnit == null && convertedUnit == null) {
+            println("Conversion from ??? to ??? is impossible")
+            continue@loopcheck
+        }
+        if (originalUnit != null && convertedUnit != null) {
+            if (originalUnit.type != convertedUnit.type) {
+                println("Conversion from ${originalUnit.singular} to ${convertedUnit.singular} is impossible")
+                continue@loopcheck
+            } else {
+                // Conversion Calculus
+                conversion = originalNumber * originalUnit.ratio / convertedUnit.ratio
+                // Check if the units are singular or plural
+                val postUnitCheck = if (conversion == 1.0) convertedUnit.singular else convertedUnit.plural
+                val preUnitCheck = if (originalNumber == 1.0) originalUnit.singular else originalUnit.plural
+                // Prints final text
+                println("$originalNumber $preUnitCheck is $conversion $postUnitCheck")
+            }
+        }
+    }
+}
+
+// Function to organize userInput in 3 variables: Number Unit1 Unit2
+fun userInputSplit(userInput: String): Triple<Double?, Unidade?, Unidade?> {
+    val userInputArray = userInput.split(" ")
+
+    val originalNumber = try {
+        userInputArray[0].toDouble()
+    } catch (e: NumberFormatException) { null }
+
+    val originalUnit = try {
+        getUnit(userInputArray[1])
+    } catch (e: IllegalArgumentException) { null }
+
+    val convertedUnit = try {
+        getUnit(userInputArray[3])
+    } catch (e: IllegalArgumentException) { null }
+
+    return Triple(originalNumber, originalUnit, convertedUnit)
+}
+
+// Function to set constant from Unidade for conversion based from User Input.
+fun getUnit(unit: String): Unidade =
+    when (unit) {
+        "m", "meter", "meters" -> Unidade.METER
+        "km", "kilometer", "kilometers" -> Unidade.KILOMETER
+        "cm", "centimeter", "centimeters" -> Unidade.CENTIMETER
+        "mm", "millimeter", "millimeters" -> Unidade.MILLIMETER
+        "mi", "mile", "miles" -> Unidade.MILE
+        "yd", "yard", "yards" -> Unidade.YARD
+        "ft", "foot", "feet" -> Unidade.FOOT
+        "in", "inch", "inches" -> Unidade.INCH
+        "g", "gram", "grams" -> Unidade.GRAM
+        "kg", "kilogram", "kilograms" -> Unidade.KILOGRAM
+        "mg", "milligram", "milligrams" -> Unidade.MILLIGRAM
+        "lb", "pound", "pounds" -> Unidade.POUND
+        "oz", "ounce", "ounces" -> Unidade.OUNCE
+//        "degree celsius", "degrees celsius", "celsius", "dc", "c" -> Unit.CELSIUS
+//        "degree fahrenheit", "degrees fahrenheit", "fahrenheit", "df", "f" -> Unit.FAHRENHEIT
+//        "kelvin", "kelvins", "k" -> Unit.Kelvin
+        else -> throw IllegalArgumentException ("Wrong Unit. Try Again.")
+    }
+enum class Unidade (val short: String,
+                    val singular: String,
+                    val plural: String,
+                    val ratio: Double,
+                    val type: String
+                    ) {
+    METER("m","meter", "meters", 1.0, "Length"),
+    KILOMETER("km","kilometer", "kilometers", 1000.0, "Length"),
+    CENTIMETER("cm","centimeter", "centimeters", 0.01, "Length"),
+    MILLIMETER("mm", "millimeter", "millimeters", 0.001, "Length"),
+    MILE("mi","mile", "miles", 1609.35, "Length"),
+    YARD("yd","yard", "yards", 0.9144, "Length"),
+    FOOT("ft","foot", "feet", 0.3048, "Length"),
+    INCH("in","inch", "inches", 0.0254, "Length"),
+    GRAM("g", "gram", "grams", 1.0, "Weight"),
+    KILOGRAM("kg", "kilogram", "kilograms", 1000.0, "Weight"),
+    MILLIGRAM("mg", "milligram", "milligrams", 0.001, "Weight"),
+    POUND("lb", "pound", "pounds", 453.592, "Weight"),
+    OUNCE("oz","ounce", "ounces", 28.3495, "Weight");
+    //CELSIUS("degree Celsius", "degrees Celsius", 1.0, "Temperature"),
+    //KELVIN("Kelvin", "Kelvins", 1.0, "Temperature"),
+    //FAHRENHEIT("degree Fahrenheit", "degrees Fahrenheit", 1.0, "Temperature")
+}
---
+++ Unit Converter/task/test/Authors.kt
@@ -0,0 +1,84 @@
+
+import MeasureType.*
+import java.io.*
+import java.util.*
+
+
+enum class MeasureType {
+    Length, Weight;
+
+    fun of(short: String,
+           normal: String,
+           plural: String,
+           multiplier: Double) = Measure(this, short, normal, plural, multiplier)
+}
+
+data class Measure(
+        val type: MeasureType,
+        val short: String,
+        val normal: String,
+        val plural: String,
+        val multiplier: Double
+) {
+    fun name(amount: Double) = if (amount == 1.0) normal else plural
+}
+
+
+fun solveAuthors(sin: Scanner, sout: PrintStream) {
+
+    val measures = listOf(
+            Length.of("m", "meter", "meters", 1.0),
+            Length.of("km", "kilometer", "kilometers", 1000.0),
+            Length.of("cm", "centimeter", "centimeters", 0.01),
+            Length.of("mm", "millimeter", "millimeters", 0.001),
+            Length.of("mi", "mile", "miles", 1609.35),
+            Length.of("yd", "yard", "yards", 0.9144),
+            Length.of("ft", "foot", "feet", 0.3048),
+            Length.of("in", "inch", "inches", 0.0254),
+
+            Weight.of("g", "gram", "grams", 1.0),
+            Weight.of("kg", "kilogram", "kilograms", 1000.0),
+            Weight.of("mg", "milligram", "milligrams", 0.001),
+            Weight.of("lb", "pound", "pounds", 453.592),
+            Weight.of("oz", "ounce", "ounces", 28.3495)
+    )
+
+    val namesToMeasures = measures
+            .flatMap { m ->
+                listOf(m.short, m.normal, m.plural).map { name -> name to m }
+            }.toMap()
+
+
+    while (true) {
+        sout.print("Enter what you want to convert (or exit): ")
+        val valueStr = sin.next()
+        if (valueStr == "exit") {
+            break
+        }
+
+        val value = valueStr.toDouble()
+
+        // read measures:
+        val m1Str = sin.next().toLowerCase()
+        val m1 = namesToMeasures[m1Str]
+        sin.next() // unknown word like to or in
+        val m2Str = sin.next().toLowerCase()
+        val m2 = namesToMeasures[m2Str]
+
+
+        /**
+         * error handlers from stage5:
+         */
+        if (m1 == null || m2 == null) {
+            sout.println("Conversion from ${m1?.plural ?: "???"} to ${m2?.plural ?: "???"} is impossible")
+            continue
+        }
+        if (m1.type != m2.type) {
+            sout.println("Conversion from ${m1.plural} to ${m2.plural} is impossible")
+            continue
+        }
+
+        val converted = value * m1.multiplier / m2.multiplier
+        sout.println("$value ${m1.name(value)} is $converted ${m2.name(converted)}")
+    }
+}
